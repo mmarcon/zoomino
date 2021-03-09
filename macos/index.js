52425ae9fb8b8b5f4eaea845bc4a0126c386ef59
Massimiliano Marcon <me@marcon.me>
2021-03-09 20:38:17 +0100

    PoC

---
+++ macos/index.js
@@ -0,0 +1,49 @@
+import {runAppleScriptAsync} from 'run-applescript';
+
+const unmute = `
+-- Function definitions
+-- https://hints.macworld.com/article.php?story=20060921045743404
+
+on menu_click(mList)
+	local appName, topMenu, r
+	
+	-- Validate our input
+	if mList's length < 3 then error "Menu list is not long enough"
+	
+	-- Set these variables for clarity and brevity later on
+	set {appName, topMenu} to (items 1 through 2 of mList)
+	set r to (items 3 through (mList's length) of mList)
+	
+	-- This overly-long line calls the menu_recurse function with
+	-- two arguments: r, and a reference to the top-level menu
+	tell application "System Events" to my menu_click_recurse(r, ((process appName)'s ¬
+		(menu bar 1)'s (menu bar item topMenu)'s (menu topMenu)))
+end menu_click
+
+on menu_click_recurse(mList, parentObject)
+	local f, r
+	
+	-- 'f' = first item, 'r' = rest of items
+	set f to item 1 of mList
+	if mList's length > 1 then set r to (items 2 through (mList's length) of mList)
+	
+	-- either actually click the menu item, or recurse again
+	tell application "System Events"
+		if mList's length is 1 then
+			click parentObject's menu item f
+		else
+			my menu_click_recurse(r, (parentObject's (menu item f)'s (menu f)))
+		end if
+	end tell
+end menu_click_recurse
+
+-- Actual code
+
+set zoom to "zoom.us"
+tell application zoom to activate
+menu_click({zoom, "Meeting", "Unmute Audio"})
+`
+
+const result = await runAppleScriptAsync(unmute);
+
+console.log(result);
