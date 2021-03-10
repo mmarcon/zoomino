937af3f0496367c9ad92e8f8edd8ba070a195abd
Massimiliano Marcon <me@marcon.me>
2021-03-10 18:01:29 +0100

    Adjusted pins

--- arduino/arduino/arduino.ino
+++ arduino/arduino/arduino.ino
@@ -1,7 +1,7 @@
-const int LED1 = 2;
-const int LED2 = 4;
-const int BUTTON1 = 8;
-const int BUTTON2 = 12;
+const int LED_MUTE = 12;
+const int LED_UNMUTE = 4;
+const int BUTTON_MUTE = 8;
+const int BUTTON_UNMUTE = 2;
 int greenState = 0;
 int redState = 0;
 
@@ -9,37 +9,37 @@ char buff[64];
 
 void setup()
 {
-  pinMode(LED1, OUTPUT);
-  pinMode(LED2, OUTPUT);
-  pinMode(BUTTON1, OUTPUT);
-  pinMode(BUTTON2, INPUT);
-  digitalWrite(LED1, LOW);
-  digitalWrite(LED2, LOW);
+  pinMode(LED_UNMUTE, OUTPUT);
+  pinMode(LED_MUTE, OUTPUT);
+  pinMode(BUTTON_UNMUTE, OUTPUT);
+  pinMode(BUTTON_MUTE, INPUT);
+  digitalWrite(LED_UNMUTE, LOW);
+  digitalWrite(LED_MUTE, LOW);
   Serial.begin(9600);
 }
 
 void loop()
 {
   
-  greenState = digitalRead(BUTTON1);
-  redState = digitalRead(BUTTON2);
+  greenState = digitalRead(BUTTON_UNMUTE);
+  redState = digitalRead(BUTTON_MUTE);
 
   sprintf(buff, "%d|%d", greenState, redState);
   Serial.println(buff);
   
   if (greenState == HIGH)
   {
-    digitalWrite(LED1, HIGH);
+    digitalWrite(LED_UNMUTE, HIGH);
   } 
   else {
-    digitalWrite(LED1, LOW);
+    digitalWrite(LED_UNMUTE, LOW);
   }
   
   if (redState == HIGH)
   {
-    digitalWrite(LED2, HIGH);
+    digitalWrite(LED_MUTE, HIGH);
   } 
   else{
-    digitalWrite(LED2, LOW);
+    digitalWrite(LED_MUTE, LOW);
   }
 }
